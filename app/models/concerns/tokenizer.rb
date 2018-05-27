1d9a34df6698d029cd315aa62122b0b77921a6b1
vaixellx <boat.pathompon@gmail.com>
2018-05-28 00:48:47 +0700

    Make tokenize able to receive token length

--- app/models/concerns/tokenizer.rb
+++ app/models/concerns/tokenizer.rb
@@ -7,8 +7,9 @@ module Tokenizer
   ].flatten.freeze
 
   class_methods do
-    def tokenize(column=:token)
+    def tokenize(column=:token, length: 6)
       @tokenize_column = column
+      @tokenize_token_length = length
 
       before_create :generate_token
     end
@@ -20,11 +21,15 @@ module Tokenizer
     self.class.instance_variable_get('@tokenize_column')
   end
 
+  def tokenize_token_length
+    self.class.instance_variable_get('@tokenize_token_length')
+  end
+
   def generate_token
     self.send "#{tokenize_column}=", random_token
   end
 
   def random_token
-    Array.new(6) { TOKEN_FRAGMENTS.sample }.join
+    Array.new(tokenize_token_length) { TOKEN_FRAGMENTS.sample }.join
   end
 end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,19 +1,6 @@
 class Invitation < ApplicationRecord
-  TOKEN_FRAGMENTS = [
-    ('A'..'Z').to_a - ['I', 'O'],
-    (2..9).to_a
-  ].flatten.freeze
+  include Tokenizer
 
+  tokenize length: 10
   validates :email, presence: true
-  before_create :generate_token
-
-  private
-
-  def generate_token
-    self.ref_code = random_token
-  end
-
-  def random_token
-    Array.new(10) { TOKEN_FRAGMENTS.sample }.join
-  end
 end
